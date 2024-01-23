1b02063aeba5f054e36f4642763e2b69df834f4f
Jake Steele <jake.steele@frozenmountain.com>
2024-01-22 16:51:10 -0800

    traded speed for readability

--- merged-sort-arrays.ts
+++ merged-sort-arrays.ts
@@ -3,24 +3,24 @@
  * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
  * Do not return anything, modify nums1 in-place instead.
  */
- function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let pointer1 = m - 1; // Pointer position for nums1.
-    let pointer2 = n - 1; // Pointer position for nums2.
-    for (var i = (m + n) - 1; i >= 0; i--) { // Check through array items.
-        if (pointer1 < 0) {  // If the first array is out of items.
-            nums1[i] = nums2[pointer2];
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    let pointer1 : number = m - 1; // Pointer position for nums1.
+    let pointer2 : number = n - 1; // Pointer position for nums2.
+    let pointer : number = (m + n) - 1; // Position of input back into nums1.
+    for (;pointer >= 0; pointer--) { // check for each array position in nums1
+        if (pointer1 < 0) { 
+            nums1[pointer] = nums2[pointer2];
             pointer2--;
         }
-        else if (pointer2 < 0) { // If the second array is out of items.
-            nums1[i] = nums1[pointer1];
+        else if (pointer2 < 0) {
+            nums1[pointer] = nums1[pointer1];
             pointer1--;
         }
-         // compare the next two items in both arrays.
         else if (nums1[pointer1] >= nums2[pointer2]) {
-            nums1[i] = nums1[pointer1];
+            nums1[pointer] = nums1[pointer1];
             pointer1--;
         } else if (nums2[pointer2] > nums1[pointer1]) {
-            nums1[i] = nums2[pointer2];
+            nums1[pointer] = nums2[pointer2];
             pointer2--;
         }
     }
